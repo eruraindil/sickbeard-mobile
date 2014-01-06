5fbcd1e1875aed468d32d01cca2dd2e38c86e414
Matthew Roberts <mrober13@uoguelph.ca>
2014-01-06 13:43:37 -0700

    making redirect page responsive

--- redirect.php
+++ redirect.php
@@ -9,6 +9,22 @@ if ( $dst ):?>
 <!DOCTYPE html>
 <html>
     <header>
+        <meta charset="utf-8">
+        <title>SickBeard Mobile - <?php echo $this->name; ?></title>
+        <meta name="description" content="">
+        <meta name="HandheldFriendly" content="True">
+        <meta name="MobileOptimized" content="320">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <meta http-equiv="cleartype" content="on">
+        <link rel="apple-touch-icon-precomposed" sizes="144x144" href="img/touch/apple-touch-icon-144x144-precomposed.png">
+        <link rel="apple-touch-icon-precomposed" sizes="114x114" href="img/touch/apple-touch-icon-114x114-precomposed.png">
+        <link rel="apple-touch-icon-precomposed" sizes="72x72" href="img/touch/apple-touch-icon-72x72-precomposed.png">
+        <link rel="apple-touch-icon-precomposed" href="img/touch/apple-touch-icon-57x57-precomposed.png">
+        <link rel="shortcut icon" href="img/touch/apple-touch-icon.png">
+        <!-- Tile icon for Win8 (144x144 + tile color) -->
+        <meta name="msapplication-TileImage" content="img/touch/apple-touch-icon-144x144-precomposed.png">
+        <meta name="msapplication-TileColor" content="#222222">
+        
         <link rel="stylesheet" href="css/normalize.css">
         <link rel="stylesheet" href="themes/sickbeard-mobile.min.css" />
         <link rel="stylesheet" href="themes/jquery.mobile.icons.min.css" />
