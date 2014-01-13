90ea3556ebdfc70450597c23486a307e0e9f3aa0
Matthew Roberts <mrober13@uoguelph.ca>
2014-01-13 18:24:30 -0500

    bug fix to shows refresh

--- forceUpdate.php
+++ forceUpdate.php
@@ -5,7 +5,8 @@ if(isset($_GET['id'])) {
     unlink("cache/getShow_$_GET[id].json");
     header("Location: redirect?to=./?id=$_GET[id]");
 } else {
-    unlink("cache/getShows.json");
+    unlink("cache/getShows_byname.json");
+    unlink("cache/getShows_byid.json");
     header("Location: redirect?to=./");
 }
 ?>
