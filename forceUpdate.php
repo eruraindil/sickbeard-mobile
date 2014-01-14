5f8e8e72082aca543784705feeb1503cd60fb0fa
Matthew Roberts <mrober13@uoguelph.ca>
2014-01-13 23:13:49 -0500

    refresh thumbs on show refresh

--- forceUpdate.php
+++ forceUpdate.php
@@ -1,8 +1,19 @@
 <?php
 namespace SickBeardMobile;
 
+require_once('global.php');
+
 if(isset($_GET['id'])) {
+    global $SB_PAGE_THUMB_W;
+    global $SB_PAGE_THUMB_H;
+    global $SB_LIST_THUMB_W;
+    global $SB_LIST_THUMB_H;
+    
     unlink("cache/getShow_$_GET[id].json");
+    unlink("cache/thumbs/$_GET[id]_$SB_LIST_THUMB_W" . "x$SB_LIST_THUMB_H.jpg");
+    unlink("cache/thumbs/$_GET[id]_$SB_PAGE_THUMB_W" . "x$SB_PAGE_THUMB_H.jpg");
+    unlink("cache/thumbs/$_GET[id].jpg");
+    
     header("Location: redirect?to=./?id=$_GET[id]");
 } else {
     unlink("cache/getShows_byname.json");
